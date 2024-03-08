f6737ee1df5bac5067df58700fc2a9f336531c13
TeiturArgeSeloy <130432976+TeiturArgeSeloy@users.noreply.github.com>
2024-03-08 16:18:59 +0100

    Set target path

---
+++ proxies/live/apiproxy/resources/jsc/WritePathToVariable.js
@@ -0,0 +1,5 @@
+
+// If we don't set this then it will add the "/FHIR/R4/DocumentReference"
+context.setVariable("target.copy.pathsuffix", false);
+// Write the new path into the variable '/spine-api-gateway/validate'
+context.setVariable("target_path", "/spine-api-gateway/validate");
